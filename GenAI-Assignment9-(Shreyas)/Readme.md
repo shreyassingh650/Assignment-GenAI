b55a5c11a1b60e95d2d256f2ce0ba56005838bdf
shreyassingh650 <shreyas.singh650@gmail.com>
2026-09-07 20:47:13 +0530

    .

---
+++ GenAI-Assignment9-(Shreyas)/Readme.md
@@ -0,0 +1,9 @@
+# My Streamlit Project
+
+## How to Run
+
+1. Install Streamlit:
+   pip install streamlit
+
+2. Run the app:
+   streamlit run app.py
---
+++ GenAI-Assignment9-(Shreyas)/Task 1/app_basic.py
@@ -0,0 +1,6 @@
+#task 1
+import numpy as np
+arr = np.array([[1,2,3],[4,5,6]])
+# random = np.random.random((4,4))*100.ṇastype('int32')
+print(np.median(arr))
+# print((random ))
