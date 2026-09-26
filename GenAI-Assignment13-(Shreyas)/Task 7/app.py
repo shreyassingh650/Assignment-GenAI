3ae880ebc3407a7a53bdf85da2dc76cea7e11b16
shreyassingh650 <shreyas.singh650@gmail.com>
2026-09-26 22:25:00 +0530

    .

--- GenAI-Assignment13-(Shreyas)/Task 7/app.py
+++ GenAI-Assignment13-(Shreyas)/Task 7/app.py
@@ -1,1 +1,20 @@
 #Task 7 
+import pandas as pd 
+from sklearn.preprocessing import OneHotEncoder
+
+df = pd.read_csv('../performance.csv')
+df = df[['Hours_Studied', 'Attendance', 'Parental_Involvement',
+       'Access_to_Resources', 'Sleep_Hours', 'Motivation_Level', 'Internet_Access',
+        'Family_Income', 'Teacher_Quality', 'Gender',
+       'Exam_Score']]
+#handling missing values
+df['Teacher_Quality'] = df['Teacher_Quality'].astype('category')
+df['Teacher_Quality'] = df['Teacher_Quality'].fillna(df['Teacher_Quality'].mode()[0])
+
+oh = OneHotEncoder(sparse_output=False)
+temp = pd.DataFrame(
+    oh.fit_transform(df[['Teacher_Quality']]),
+    columns=oh.get_feature_names_out()
+)
+
+print(temp)
